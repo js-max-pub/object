1402d80229c15219acc737048ae7b424cbd4d969
Max Fechner <max.fechner@gmail.com>
2021-03-23 10:18:41 +0100

    - added extend function

--- src.js
+++ src.js
@@ -1,3 +1,13 @@
 export function copy(o) {
 	return JSON.parse(JSON.stringify(o))
 }
+
+
+// export function extend(object, functions = [], getter = [], setter = []) {
+export function extend(object, ...p) {
+	// p == functions, getter, setter
+	let n = ['value', 'get', 'set']
+	for (let i in p)
+		for (let f of p[i] ?? [])
+			Object.defineProperty(object.prototype, f.name, { [n[i]]: function (...p) { return f(this, ...p) } })
+}
